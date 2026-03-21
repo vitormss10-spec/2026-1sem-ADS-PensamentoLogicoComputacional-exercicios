b4c6c01ae8e66519e1f3a38d5de68f7ef01c18b0
vitormss10-spec <vitormss10@gmail.com>
2026-03-21 13:45:17 -0300

    Update exercicio3.py

--- 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-3/exercicio3.py
+++ 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-3/exercicio3.py
@@ -1,1 +1,16 @@
 #exercicio3
+ numero = int(input("digite um numero inteiro: "))
+digite um numero inteiro: 8
+>>> for i in  range (1, 11):
+...     print (f"{numero} x {i} = {numero * i}")
+...
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+8 x 4 = 32
+8 x 5 = 40
+8 x 6 = 48
+8 x 7 = 56
+8 x 8 = 64
+8 x 9 = 72
+8 x 10 = 80
