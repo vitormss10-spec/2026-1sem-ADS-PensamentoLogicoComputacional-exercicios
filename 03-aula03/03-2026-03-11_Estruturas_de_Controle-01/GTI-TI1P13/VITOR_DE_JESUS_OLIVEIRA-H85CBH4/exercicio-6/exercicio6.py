3874d17983d6eae416e08dbbee060fdb55120ff1
vitormss10-spec <vitormss10@gmail.com>
2026-03-21 14:10:47 -0300

    Update exercicio6.py

--- 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-6/exercicio6.py
+++ 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-6/exercicio6.py
@@ -1,1 +1,9 @@
 #exercicio6
+numero = int(input("digite um numero inteiro: "))
+digite um numero inteiro: 7
+>>> if numero % 2 == 0:
+...    print ( "numero par")
+... else:
+...    print ("numero ímpar")
+...
+numero ímpar
