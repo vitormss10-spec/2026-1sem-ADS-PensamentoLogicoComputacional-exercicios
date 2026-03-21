ef27c61acc34d7b2a13de41bc72b3a8f76505579
vitormss10-spec <vitormss10@gmail.com>
2026-03-21 14:36:54 -0300

    Implement product pricing and discount logic
    não consegui realizar esse exercicio professor!.

--- 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-8/exercicio8.py
+++ 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-8/exercicio8.py
@@ -1,1 +1,11 @@
 #exercicio8
+quantidade = int(input("digite a quantidade de produtos: "))
+digite a quantidade de produtos: 11
+>>> preço = 100
+>>> total = quantidade * preço
+>>> if quantidade >= 10:
+...     total = total * 0.9
+...     print (f" valor final: R$ {total: .2f"})
+...
+  File "<python-input-3>", line 3
+    print (f" valor final: R$ {total: .2f"})
