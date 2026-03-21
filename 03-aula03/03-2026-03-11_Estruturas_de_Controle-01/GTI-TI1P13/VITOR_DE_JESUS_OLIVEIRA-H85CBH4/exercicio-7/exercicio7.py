5fd9836fa30a9edabf1f1110eff627f1a4f9f475
vitormss10-spec <vitormss10@gmail.com>
2026-03-21 14:14:59 -0300

    Update exercicio7.py

--- 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-7/exercicio7.py
+++ 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-7/exercicio7.py
@@ -1,1 +1,11 @@
 #exercicio7
+temperatura = float(input("digite a temperatura: "))
+digite a temperatura: 20
+>>> if temperatura < 15:
+...    print ("frio")
+... elif 15 <= temperatura <= 25:
+...    print ("agradavel")
+... else:
+...    print ("quente")
+...
+agradavel
