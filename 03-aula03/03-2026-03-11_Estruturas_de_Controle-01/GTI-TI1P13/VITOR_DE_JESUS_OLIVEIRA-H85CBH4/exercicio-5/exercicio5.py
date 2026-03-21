6b54746ef09b558256757d2015a4aed92a590707
vitormss10-spec <vitormss10@gmail.com>
2026-03-21 14:01:11 -0300

    Add age check for access permission

--- 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-5/exercicio5.py
+++ 03-aula03/03-2026-03-11_Estruturas_de_Controle-01/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-5/exercicio5.py
@@ -1,1 +1,10 @@
 #exercicio5
+idade = int (input("digite a idade: "))
+digite a idade: 21
+>>> if idade >= 18:
+...     print ("acesso liberado")
+... else:
+...     print ("acesso negado")
+...
+...
+acesso liberado
