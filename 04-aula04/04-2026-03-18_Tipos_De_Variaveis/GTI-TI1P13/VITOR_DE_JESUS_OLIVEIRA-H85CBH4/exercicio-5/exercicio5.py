95ec812688d6cc83f06b214b72f64ab893e56317
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 13:20:58 -0300

    Add input handling for decimal to int conversion

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-5/exercicio5.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-5/exercicio5.py
@@ -1,1 +1,9 @@
 #exercicio5
+numero_str = input("digite um numero decimal: ")
+digite um numero decimal: 10.20
+>>> numero_float = float(numero_str)
+>>> numero_int = int(numero_float)
+>>> print("float:" , numero_float)
+float: 10.2
+>>> print("int: " , numero_int)
+int:  10
