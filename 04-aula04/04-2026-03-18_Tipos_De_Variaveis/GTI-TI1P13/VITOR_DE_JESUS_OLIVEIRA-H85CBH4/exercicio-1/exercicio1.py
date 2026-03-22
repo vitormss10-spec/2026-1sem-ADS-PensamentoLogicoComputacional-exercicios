7d21351fd5589e2a8a631aba785ef0af19bd1f71
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 12:45:35 -0300

    Add arithmetic operations in exercicio1.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-1/exercicio1.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-1/exercicio1.py
@@ -1,1 +1,19 @@
 #exercicio1
+numero1 = input("digite o primeiro numero: ")
+digite o primeiro numero: 2
+>>> numero2 = input("digite o segundo numero: ")
+digite o segundo numero: 4
+>>> numero1 = int(numero1)
+>>> numero2 = int(numero2)
+>>> soma = numero1 + numero2
+>>> subtração = numero1 - numero2
+>>> divisão = numero1 / numero2
+>>> print("soma" , soma)
+soma 6
+>>> print("subtração" , subtração)
+subtração -2
+>>> print("divisão" , divisão)
+divisão 0.5
+>>> resto = numero1 % numero2
+>>> print("resto" , resto)
+resto 2
