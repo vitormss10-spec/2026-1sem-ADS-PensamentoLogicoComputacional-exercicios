348f8ffb2fe3617fd07d840cabb1de59c3f08b3d
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 12:55:28 -0300

    Add input and comparison logic in exercicio2.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-2/exercicio2.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-2/exercicio2.py
@@ -1,1 +1,13 @@
 #exercicio2
+numero1 = input("digite o numero1: ")
+digite o numero1: 5
+>>> numero2 = input("digite o numero2: ")
+digite o numero2: 7
+>>> if numero1 == numero2:
+...     print("os numeros são iguais")
+... elif numero1 > numero2:
+...     print("o maior numero é", numero1)
+... else:
+...     print("o maior numero é" , numero2)
+...
+o maior numero é 7
