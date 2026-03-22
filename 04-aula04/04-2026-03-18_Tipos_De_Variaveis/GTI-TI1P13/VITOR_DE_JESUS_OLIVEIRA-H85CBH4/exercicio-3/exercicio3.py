f33a4d7b6d00cd06daf301775399de5541226a38
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 13:02:23 -0300

    Add number input and range check in exercicio3.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-3/exercicio3.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-3/exercicio3.py
@@ -1,1 +1,9 @@
 #exercicio3
+numero = int(input("digite um numero: "))
+digite um numero: 40
+>>> if numero >= 10 and numero <= 50:
+...     print("esta entre 10 e 50")
+... else:
+...     print ("não esta entre 10 e 50")
+...
+esta entre 10 e 50
