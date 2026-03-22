d4fba04fbff1e3b9e83b093a22a04fe566f29a1d
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 13:13:13 -0300

    Update exercicio4.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-4/exercicio4.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-4/exercicio4.py
@@ -1,1 +1,11 @@
 #exercicio4
+>>> idade = int(input("digite a idade: " ))
+digite a idade: 56
+>>> if idade >= 60:
+...     print("é idosa")
+... elif idade >= 18:
+...     print ("é maior de idade: ")
+... else:
+...     print("É menor de idade")
+...
+é maior de idade:
