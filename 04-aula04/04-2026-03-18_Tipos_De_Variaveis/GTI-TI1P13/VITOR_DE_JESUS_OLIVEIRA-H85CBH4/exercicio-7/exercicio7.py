b884edb8fe8cdd33faf29e9a5a79e5600c3b5ad1
vitormss10-spec <vitormss10@gmail.com>
2026-03-22 13:27:47 -0300

    Update exercicio7.py

--- 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-7/exercicio7.py
+++ 04-aula04/04-2026-03-18_Tipos_De_Variaveis/GTI-TI1P13/VITOR_DE_JESUS_OLIVEIRA-H85CBH4/exercicio-7/exercicio7.py
@@ -1,1 +1,10 @@
 #exercicio7
+lista1 = [2,4,5]
+>>> lista2 = [2,5,2]
+>>> print( lista1 == lista2)
+False
+>>> print(lista1 is lista2)
+False
+== é para verificar se os numeros são iguais. True
+is é para verificar se são o mesmo objeto de memoria
+diferentes = False 
